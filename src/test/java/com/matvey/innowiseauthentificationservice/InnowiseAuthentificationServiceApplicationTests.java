98c4c9b3cc20e66f7a01ef0c310c6215b68f7caa
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-05-26 00:51:24 +0300

    init commit

---
+++ src/test/java/com/matvey/innowiseauthentificationservice/InnowiseAuthentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matvey.innowiseauthentificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseAuthentificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
